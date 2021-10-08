c0fa9316ed869ce140c9299f10854eb71e1b6927
Ricardo Salazar <no-replay>
2021-10-07 23:54:39 -0500

    add contact

---
+++ my-app/src/Contact/Contact .js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
+                <div><h2>Choose Your Package and Reserve Your Date</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Contact;
