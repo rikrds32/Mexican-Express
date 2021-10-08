994976a8f14a700ba5f217d364cdf15d679d05fb
Ricardo Salazar <no-replay>
2021-10-08 16:31:45 -0500

    erased everything add navbar and components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
 import './App.css';
-import Navigation from './components/Navbar';
-import Routes from './Routes';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Routes />
-    </div>
+     <Navbar />
   );
 }
 
 export default App;
-
-
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import './Navbar.css';
-import { Navbar, Nav, Form, Button } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
+import logo from "../mxExplogo.png";
 
-const Navigation = (props) => {
-    console.log(props);
+const Navbar = () => {
     return (
-        <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="#home">React Button</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/About">About</Nav.Link>
-                    <Nav.Link href="/Contact">Contact</Nav.Link>
-                    <Nav.Link href="/Packages">Packages</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+
+                <a className="navbar-brand" href="#"><img src={logo} alt="logo" /></a>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Packages</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
     )
 }
 
-export default withRouter(Navigation);
+export default Navbar
