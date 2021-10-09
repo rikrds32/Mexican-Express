464b3928328f838a3cd9b8bd69a3ba0f6361b3ce
Ricardo Salazar <no-replay>
2021-10-09 04:34:09 -0500

    add about section

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 const About = () => {
-    return(
+    return (
         <div className="header-wraper">
-        <div className="main-info">
-            <h1 className="h1-brand">
-               about
-            </h1><br></br>
-            <p className="p-brand">Thes best Mexican food caterign service.
-            </p>
+            <div className="main-info">
+                <p className="p-about">Mexican Express comenzo con la idea de fusionar la cultura gastronimicadel norte y sur de Mexico.
+                    Somos una compania especializada em hacer catering para tus eventos, manteniendo asi nuestro concepto
+                    de "express" pues te ofreceremos un producto de calidad, rapido y listo para ser
+                    degustado en la comodidad de tu casa o evento, pues si tu no puedes llegar a nosotros,
+                    nosotros podemos llegar a ti.
+                    No te arrepentiras cuando elijas probar nuestros productos, pues nuestro paso por diversas
+                    culturas gastronomicas nos ha ayudado a desarrollar este proyecto de la mano
+                    del Chef Rogelio Elizondo con su experiencia culinaria que desde Monterrey Nuevo Leon
+                    nos ayuda a traer los sabores de la carne a la parrilla, asi como Ricardo Salazar que
+                    con su amplio conocimiento de la comida capitalina nos aporta el secreto del verdadero sabor de Mexico.</p>
+                <p className="p-about">Mexican Express begins with the idea of a fusion between the gastronomic cultures from nort and south of Mexico.
+                    We specialize in catering for your events, kepping the idea of "express" we offer you a quality product fast and ready
+                    to be delighted in the confort of your house or your event, if you cant get to us,we can get to you.
+                    You won't regreat by elect to try our products, because our trayectory from different gastronomic cultures are resembled
+                    in this proyect by hand of our Chef Rogelio Elizondo with his experience in the food industry from Monterey Nuevo Leon
+                    brings the flavors of the grill on the other hanad Ricardo Salazar with his greate knowledge of the gastronomy in Mexico City
+                    brings the secret to truly flavor of Mexico.
+                </p>
 
+            </div>
         </div>
-    </div>
     )
 };
 export default About;
